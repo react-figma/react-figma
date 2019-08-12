b81a149865d598902bb9fec320fa31fa584c71b0
Yaroslav Losev <losevyaroslav@gmail.com>
2019-08-12 14:25:58 +0300

    Implement basic text render

--- src/code.tsx
+++ src/code.tsx
@@ -3,10 +3,6 @@ import { renderer } from './renderer';
 import { Rectangle } from './components/rectangle/Rectangle';
 import { Text } from './components/text/Text';
 
-renderer(
-    <Rectangle style={{ width: 200, height: 100, backgroundColor: '#dd55aa' }}>
-        <Text style={{ color: '#ffffff' }}>text</Text>
-    </Rectangle>
-);
+renderer(<Text style={{ color: '#ff0000' }}>text</Text>);
 
 figma.closePlugin();
--- src/components/text/Text.tsx
+++ src/components/text/Text.tsx
@@ -1,5 +1,30 @@
 import * as React from 'react';
+import { colorToRGB } from '../../helpers/color';
 
-export const Text = props => {
-    return <></>;
-};
+export class Text extends React.PureComponent<{ style: any }> {
+    static async drawFigma(props) {
+        const textNode = figma.createText();
+
+        let fontName = textNode.fontName;
+        if (typeof fontName !== 'object') {
+            fontName = {
+                family: 'Roboto',
+                style: 'Regular'
+            };
+        }
+        await figma.loadFontAsync(fontName);
+
+        textNode.characters = props.children;
+
+        const { color } = props.style;
+        if (color) {
+            textNode.fills = [{ type: 'SOLID', color: colorToRGB(color) }];
+        }
+
+        figma.currentPage.appendChild(textNode);
+    }
+
+    render() {
+        return <></>;
+    }
+}
