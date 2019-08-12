484e62cbc2172faa9fd2bcd242746c4aa225087d
Ilia Lesik <ilialesik@gmail.com>
2019-08-12 09:48:52 +0300

    Create Text component

--- src/code.tsx
+++ src/code.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 import {renderer} from "./renderer";
 import {Rectangle} from "./components/rectangle/Rectangle";
+import {Text} from "./components/text/Text";
 
-renderer(<Rectangle />);
+renderer(
+    <Rectangle style={{width: 100, height: 100}}>
+        <Text style={{color: "#ff0000"}}>text</Text>
+    </Rectangle>);
 
 figma.closePlugin();
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { convertToJSON } from 'react-json-renderer'
 
 export const renderer = (jsx: React.ReactNode) => {
+    debugger;
     const renderedTree = convertToJSON(jsx);
 
     console.log(renderedTree);
