21ff04050029c7dfbab93e3eac620a672277e1e5
Ilia Lesik <ilialesik@gmail.com>
2019-08-14 23:14:57 +0300

    Refactor renderer

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,27 +1,18 @@
 import * as React from 'react';
 import * as renderers from './renderers';
 
-export const renderer = async (jsx: React.ReactElement<any>) => {
-    const queue = [];
-
-    const collectQueue = jsx => {
-        if (typeof jsx.type === 'function') {
-            const result = jsx.type(jsx.props);
-            collectQueue(result);
-        } else if (jsx.type === React.Fragment) {
-            React.Children.forEach(jsx.props.children, child => {
-                collectQueue(child);
-            });
-        } else if (renderers[jsx.type]) {
-            queue.push(jsx);
-        }
-        if (!renderers[jsx.type] && React.isValidElement<{ children: any }>(jsx) && jsx.props.children) {
-            collectQueue(jsx.props.children);
-        }
-    };
-    collectQueue(jsx);
-
-    for (const item of queue) {
-        await renderers[item.type](item.props);
+export const renderer = async (jsx: any) => {
+    if (typeof jsx.type === 'function') {
+        const result = jsx.type(jsx.props);
+        await renderer(result);
+    } else if (jsx.type === React.Fragment) {
+        React.Children.forEach(jsx.props.children, async child => {
+            await renderer(child);
+        });
+    } else if (renderers[jsx.type]) {
+        await renderers[jsx.type](jsx.props);
+    }
+    if (!renderers[jsx.type] && React.isValidElement<{ children: any }>(jsx) && jsx.props.children) {
+        await renderer(jsx.props.children);
     }
 };
