4b0fa1a33da41d79e2f22bfc7d6c6cf222216bea
Yaroslav Losev <losevyaroslav@gmail.com>
2019-08-16 17:40:44 +0300

    Implement frame component

--- jsx-interfaces.d.ts
+++ jsx-interfaces.d.ts
@@ -1,12 +1,13 @@
-import {RectangleProps} from "./src/renderers/rectangle";
-
+import { RectangleProps } from './src/renderers/rectangle';
+import { FrameProps } from './src/components/frame/Frame';
 
 declare global {
     namespace JSX {
         interface IntrinsicElements {
-            currentPage: any,
-            page: any,
-            rectangle: RectangleProps
+            currentPage: any;
+            page: any;
+            rectangle: RectangleProps;
+            frame: FrameProps;
         }
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { Text } from './components/text/Text';
 export { Rectangle } from './components/rectangle/Rectangle';
 export { Page } from './components/page/Page';
 export { CurrentPage } from './components/page/CurrentPage';
+export { Frame } from './components/frame/Frame';
