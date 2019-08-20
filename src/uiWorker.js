9469f824a40da5e0ddec6da2a856dd9031200d58
Ilia Lesik <ilialesik@gmail.com>
2019-08-20 22:16:23 +0300

    Remove uiWorker.js

--- src/uiWorker.js
+++
@@ -1,7 +0,0 @@
-import { yogaWorker } from './workers/yogaWorker';
-import { base64ToBlobWorker } from './workers/base64ToBlobWorker';
-export const uiWorker = ({ yoga }) => event => {
-    const message = event.data.pluginMessage;
-    yogaWorker(yoga)(message);
-    base64ToBlobWorker()(message);
-};
