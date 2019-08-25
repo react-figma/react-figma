3adfa13114e64d37df145a86c39171061ea79631
Ilia Lesik <ilialesik@gmail.com>
2019-08-25 13:06:53 +0300

    Remove all examples except design-system

--- examples/button/src/code.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { renderer } from '../../../src/';
-import { App } from './App';
-
-(async () => {
-    await renderer(<App />);
-    figma.closePlugin();
-})();
