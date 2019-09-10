5a2d68611aa056dfb2dd5552859d668ca2bb687a
Ilia Lesik <ilialesik@gmail.com>
2019-09-10 19:11:19 +0300

    Remove CurrentPage component

--- src/components/page/CurrentPage.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { BaseNodeProps, ChildrenProps, LayoutProps } from '../../types';
-
-interface CurrentPageProps extends ChildrenProps {}
-
-export const CurrentPage: React.ElementType<CurrentPageProps> = props => {
-    return <currentPage {...props} />;
-};
