be5c3d2b6879562d264893e40380da41a41f4a78
Ilia Lesik <ilialesik@gmail.com>
2019-09-10 19:14:43 +0300

    Fix basic example

--- examples/basic/src/App.tsx
+++ examples/basic/src/App.tsx
@@ -1,19 +1,10 @@
 import * as React from 'react';
-import { Frame, Page, Rectangle, Text, Component } from '../../../src';
+import { Page, Rectangle } from '../../../src';
 
 export const App = () => {
     return (
-        <Page name="Design system">
-            <Frame>
-                <Component>
-                    <Rectangle style={{ width: 200, height: 100, backgroundColor: '#12ff00' }} />
-                </Component>
-                <Rectangle style={{ width: 200, height: 100, backgroundColor: '#00ffe4' }} />
-                <Component>
-                    <Rectangle style={{ width: 200, height: 100, backgroundColor: '#ff0001' }} />
-                </Component>
-                <Rectangle style={{ width: 200, height: 100, backgroundColor: '#7000ff' }} />
-            </Frame>
+        <Page name="New page">
+            <Rectangle style={{ width: 200, height: 100, backgroundColor: '#12ff00' }} />
         </Page>
     );
 };
