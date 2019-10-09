907b752b8fef607b59e51647070db86e3be0e98a
Ilia Lesik <ilialesik@gmail.com>
2019-10-09 23:49:28 +0300

    Add rerender test

--- src/__tests__/renderer.test.tsx
+++ src/__tests__/renderer.test.tsx
@@ -2,25 +2,72 @@ import * as React from 'react';
 import { render } from '../renderer';
 import { Rectangle } from '..';
 
+function applyMixins(derivedCtor: any, baseCtors: any[]) {
+    baseCtors.forEach(baseCtor => {
+        Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
+            Object.defineProperty(
+                derivedCtor.prototype,
+                name,
+                Object.getOwnPropertyDescriptor(baseCtor.prototype, name)
+            );
+        });
+    });
+}
+
 class Children {
     children: Array<any>;
-    constructor() {
-        this.children = [];
-    }
     appendChild(item) {
+        if (!this.children) {
+            this.children = [];
+        }
         this.children.push(item);
     }
 }
 
-// @ts-ignore
-global.figma = {
+class Base {
+    private pluginData: { [key: string]: string };
+
+    setPluginData(key: string, value: string) {
+        if (!this.pluginData) {
+            this.pluginData = {};
+        }
+        this.pluginData[key] = value;
+    }
+    getPluginData(key: string) {
+        if (!this.pluginData) {
+            return;
+        }
+        return this.pluginData[key];
+    }
+}
+
+class Rectangle1 {
+    type = 'RECTANGLE';
+
+    resize() {}
+}
+
+applyMixins(Rectangle1, [Base]);
+
+const createFigma = () => ({
     root: new Children(),
-    createRectangle: () => ({ type: 'RECTANGLE', setPluginData: () => {}, resize: () => {} })
-};
+    createRectangle: () => new Rectangle1()
+});
 
 describe('renderer', () => {
+    beforeEach(() => {
+        // @ts-ignore
+        global.figma = createFigma();
+    });
+
     it('simple', () => {
         render(<Rectangle style={{ width: 200, height: 100, backgroundColor: '#12ff00' }} />, figma.root);
+        expect(figma.root).toMatchSnapshot();
+    });
+
+    it('simple 2', () => {
+        render(<Rectangle style={{ width: 200, height: 100, backgroundColor: '#12ff00' }} />, figma.root);
+        render(<Rectangle style={{ width: 200, height: 100, backgroundColor: '#ff3500' }} />, figma.root);
         expect(figma.root).toMatchSnapshot();
     });
 });
