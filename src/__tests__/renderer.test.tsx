c9e0375235aa60721a42cce0bec4c245fda31998
Ilia Lesik <ilialesik@gmail.com>
2019-10-10 20:11:30 +0300

    Install figma-api-stub
    User figma-api-stub at renderer test

--- src/__tests__/renderer.test.tsx
+++ src/__tests__/renderer.test.tsx
@@ -1,58 +1,7 @@
 import * as React from 'react';
 import { render } from '../renderer';
 import { Rectangle } from '..';
-
-function applyMixins(derivedCtor: any, baseCtors: any[]) {
-    baseCtors.forEach(baseCtor => {
-        Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
-            Object.defineProperty(
-                derivedCtor.prototype,
-                name,
-                Object.getOwnPropertyDescriptor(baseCtor.prototype, name)
-            );
-        });
-    });
-}
-
-class Children {
-    children: Array<any>;
-    appendChild(item) {
-        if (!this.children) {
-            this.children = [];
-        }
-        this.children.push(item);
-    }
-}
-
-class Base {
-    private pluginData: { [key: string]: string };
-
-    setPluginData(key: string, value: string) {
-        if (!this.pluginData) {
-            this.pluginData = {};
-        }
-        this.pluginData[key] = value;
-    }
-    getPluginData(key: string) {
-        if (!this.pluginData) {
-            return;
-        }
-        return this.pluginData[key];
-    }
-}
-
-class Rectangle1 {
-    type = 'RECTANGLE';
-
-    resize() {}
-}
-
-applyMixins(Rectangle1, [Base]);
-
-const createFigma = () => ({
-    root: new Children(),
-    createRectangle: () => new Rectangle1()
-});
+import { createFigma } from 'figma-api-stub';
 
 describe('renderer', () => {
     beforeEach(() => {
