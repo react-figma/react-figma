fb2edaf666a9fe10899cfc84c3b0219250848ecf
Yaroslav Losev <losevyaroslav@gmail.com>
2019-11-18 16:57:55 +0300

    Implement reconciler skeleton

--- src/reconciler/reconciler.ts
+++ src/reconciler/reconciler.ts
@@ -1,3 +1,23 @@
 import * as Reconciler from 'react-reconciler';
 
-export const render = async (jsx: any, rootNode) => {};
+export const render = async (jsx: any, rootNode) => {
+    const HostConfig = {};
+
+    const reconciler = Reconciler(HostConfig);
+
+    reconciler.injectIntoDevTools({
+        bundleType: 1, // 0 for PROD, 1 for DEV
+        version: '0.1.0',
+        rendererPackageName: 'react-figma'
+    });
+
+    const container = reconciler.createContainer(
+        rootNode, // container
+        true, // isAsync
+        true // hydrate
+    );
+
+    reconciler.updateContainer(jsx, container);
+
+    return container;
+};
