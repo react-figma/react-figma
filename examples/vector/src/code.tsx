af6d83327a7a2d7f655521b5adb09cde531c9b96
Ilia Lesik <ilialesik@gmail.com>
2019-11-20 13:06:36 +0300

    Remove manual loading fonts from all examples

--- examples/vector/src/code.tsx
+++ examples/vector/src/code.tsx
@@ -2,8 +2,5 @@ import * as React from 'react';
 import { render } from '../../../src/';
 import { App } from './App';
 
-(async () => {
-    await figma.loadFontAsync({ family: 'Roboto', style: 'Regular' });
-    render(<App />, figma.currentPage);
-    figma.closePlugin();
-})();
+render(<App />, figma.currentPage);
+figma.closePlugin();
