d8c5cc463838d2fa55b73eb641b571e8b2784aa5
Ilia Lesik <ilialesik@gmail.com>
2019-12-02 12:08:27 +0300

    Fix Nodes are positioned incorrectly with flexDirection changed after hydration #200

--- src/mixins/__tests__/layoutMixin.test.ts
+++ src/mixins/__tests__/layoutMixin.test.ts
@@ -18,4 +18,16 @@ describe('layoutMixin', () => {
         layoutMixin({ type: 'LINE', resize } as any)({ width: 500 });
         expect(resize.mock.results[0].value).toMatchSnapshot();
     });
+
+    it('apply x=0', () => {
+        const node = { type: 'RECTANGLE', x: 200 } as any;
+        layoutMixin(node)({ x: 0 });
+        expect(node.x).toEqual(0);
+    });
+
+    it('apply y=0', () => {
+        const node = { type: 'RECTANGLE', y: 100 } as any;
+        layoutMixin(node)({ y: 0 });
+        expect(node.y).toEqual(0);
+    });
 });
