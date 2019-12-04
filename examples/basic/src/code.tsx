57244dbba6d6ccdd15fbbe68ce93f3ee61e25cc7
Ilia Lesik <ilialesik@gmail.com>
2019-12-04 16:14:17 +0300

    Enable UI thread at basic example

--- examples/basic/src/code.tsx
+++ examples/basic/src/code.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
-import { render } from '../../../src/';
+import { render, subscribeOnMessages } from '../../../src/';
 import { App } from './App';
 
+figma.showUI(__html__, { visible: false });
+
+figma.ui.onmessage = message => {
+    subscribeOnMessages(message);
+};
+
 render(<App />, figma.currentPage);
-figma.closePlugin();
