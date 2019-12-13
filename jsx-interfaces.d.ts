37b9c126f5d4bfdaf913c0c95a76900cf219164c
Ilia Lesik <ilialesik@gmail.com>
2019-12-13 13:49:36 +0300

    Set component, instance interfaces at jsx-interfaces.d.ts

--- jsx-interfaces.d.ts
+++ jsx-interfaces.d.ts
@@ -3,6 +3,8 @@ import { FrameNodeProps } from './src/components/frame/Frame';
 import { TextProps } from './src/components/text/Text';
 import { GroupProps } from './src/components/group/Group';
 import { EllipseProps } from './src/components/ellipse/Ellipse';
+import {InstanceProps} from "./src/components/component/Instance";
+import {ComponentProps} from "./src/components/component/Component"
 
 declare global {
     namespace JSX {
@@ -14,9 +16,9 @@ declare global {
             group: GroupProps;
             // @ts-ignore
             text: TextProps;
-            component: any;
+            component: ComponentProps;
             ellipse: EllipseProps;
-            instance: any
+            instance: InstanceProps
         }
     }
 }
