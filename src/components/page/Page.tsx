7e64693b14c466a984e15d1bf38a4701a6af7ba2
Ilia Lesik <ilialesik@gmail.com>
2019-12-18 22:02:51 +0300

    Implement onCurrentChange prop support at page

--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -1,15 +1,38 @@
 import * as React from 'react';
-import { BaseNodeProps, ChildrenProps, LayoutProps, ExportProps, StyleOf } from '../../types';
+import { BaseNodeProps, ChildrenProps, ExportProps, StyleOf } from '../../types';
 import { useYogaLayout } from '../../hooks/useYogaLayout';
 import { YogaStyleProperties } from '../../yoga/YogaStyleProperties';
 
 export interface PageProps extends BaseNodeProps, ChildrenProps, ExportProps {
     style?: StyleOf<YogaStyleProperties>;
     isCurrent?: boolean;
+    onCurrentChange?: (isCurrent: boolean) => void;
 }
+
+const useCurrentPageChange = (nodeRef: { current?: PageNode }, callback?: (isCurrent: boolean) => void) => {
+    const didMountRef = React.useRef(false);
+    const [isCurrent, setIsCurrent] = React.useState(false);
+
+    React.useEffect(() => {
+        if (!didMountRef.current) {
+            didMountRef.current = true;
+            return;
+        }
+        callback(isCurrent);
+    }, [isCurrent]);
+
+    React.useEffect(() => {
+        const handler = () => {
+            setIsCurrent(figma.currentPage === nodeRef.current);
+        };
+        figma.on('currentpagechange', handler);
+        return () => figma.off('currentpagechange', handler);
+    }, [callback]);
+};
 
 export const Page: React.FC<PageProps> = props => {
     const nodeRef = React.useRef();
+    useCurrentPageChange(nodeRef, props.onCurrentChange);
 
     const yogaChildProps = useYogaLayout({ nodeRef, ...props });
     return <page {...props} {...yogaChildProps} innerRef={nodeRef} />;
