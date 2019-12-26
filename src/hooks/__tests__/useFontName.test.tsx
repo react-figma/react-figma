e4891b90a2bb3d0613bc8d98db6abb4bdabf32e9
Ilia Lesik <ilialesik@gmail.com>
2019-12-26 20:07:24 +0300

    Add test for useFontName

---
+++ src/hooks/__tests__/useFontName.test.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { createFigma } from 'figma-api-stub';
+import { render } from '../../renderer';
+import { wait } from '../../helpers/wait';
+import {useFontName} from "../useFontName";
+
+describe('useFontName', () => {
+    beforeEach(() => {
+        // @ts-ignore
+        global.figma = createFigma({
+            simulateErrors: true
+        });
+    });
+
+    it('useFontName searching matched fontName style', async () => {
+        const callback = jest.fn();
+        // @ts-ignore
+        figma.listAvailableFontsAsync = jest.fn(() => {
+            return Promise.resolve([
+                {
+                    fontName: {
+                        family: "Inter",
+                        style: "Regular"
+                    }
+                },
+                {
+                    fontName: {
+                        family: "Inter",
+                        style: "Semi Bold"
+                    }
+                },
+                {
+                    fontName: {
+                        family: "Inter",
+                        style: "Bold"
+                    }
+                }
+            ]);
+        });
+        const Component = () => {
+            const loadedFont = useFontName({family: "Inter", style: "SemiBold"});
+            if (!loadedFont) {
+                return null
+            }
+            callback(loadedFont);
+            return null;
+        };
+        render(<Component />, figma.currentPage);
+        await wait();
+        await wait();
+        expect(callback).toHaveBeenCalledWith({family: "Inter", style: "Semi Bold"});
+    });
+});
