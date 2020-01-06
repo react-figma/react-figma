cd0a4b3db83eee9a1abaa9329987c3dc0212f850
Ilia Lesik <ilialesik@gmail.com>
2020-01-06 10:07:02 +0300

    Implement: Gradient background should be supported at the backgroundImage #232

---
+++ src/styleTransformers/__tests__/transformGeometryStyleProperties.test.ts
@@ -0,0 +1,20 @@
+import { transformGeometryStyleProperties } from '../transformGeometryStyleProperties';
+
+describe('transformGeometryStyleProperties', () => {
+    it('backgroundColor: "red"', () => {
+        const result = transformGeometryStyleProperties('fills', { backgroundColor: 'red' });
+        expect(result).toMatchSnapshot();
+    });
+
+    it('backgroundImage: ./image.png', () => {
+        const result = transformGeometryStyleProperties('fills', { backgroundImage: './image.png' });
+        expect(result).toMatchSnapshot();
+    });
+
+    it('backgroundImage: linear-gradient(#e66465, #9198e5)', () => {
+        const result = transformGeometryStyleProperties('fills', {
+            backgroundImage: 'linear-gradient(#e66465, #9198e5)'
+        });
+        expect(result).toMatchSnapshot();
+    });
+});
