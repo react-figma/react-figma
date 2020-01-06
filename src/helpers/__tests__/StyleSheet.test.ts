d3aea7c5a02e5b6e5270f649a2efbddb7586a00c
Ilia Lesik <ilialesik@gmail.com>
2020-01-06 12:11:13 +0300

    Create test for StyleSheet.flatten

--- src/helpers/__tests__/StyleSheet.test.ts
+++ src/helpers/__tests__/StyleSheet.test.ts
@@ -35,4 +35,20 @@ describe('StyleSheet', () => {
         );
         expect(result).toMatchSnapshot();
     });
+
+    it('flatten', () => {
+        var styles = StyleSheet.create({
+            listItem: {
+                flex: 1,
+                fontSize: 16,
+                color: 'white'
+            },
+            selectedListItem: {
+                color: 'green'
+            }
+        });
+
+        const result = StyleSheet.flatten([styles.listItem, styles.selectedListItem]);
+        expect(result).toMatchSnapshot();
+    });
 });
