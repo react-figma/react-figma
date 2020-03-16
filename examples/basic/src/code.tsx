d5d54168e5f4ecd1f06b7dc8c12b0f5e2942aa93
Ilia Lesik <ilialesik@gmail.com>
2020-03-16 23:51:09 +0300

    Replace relative imports to alias at all examples

--- examples/basic/src/code.tsx
+++ examples/basic/src/code.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render, subscribeOnMessages } from '../../../src/';
+import { render, subscribeOnMessages } from 'react-figma';
 import { App } from './App';
 
 figma.showUI(__html__, { visible: false });
--- examples/dynamic-layout/src/ui.tsx
+++ examples/dynamic-layout/src/ui.tsx
@@ -1,5 +1,5 @@
 import * as yoga from 'yoga-layout-prebuilt';
-import { uiWorker } from '../../../src';
+import { uiWorker } from 'react-figma';
 
 const handler = uiWorker({ yoga, fetch });
 
