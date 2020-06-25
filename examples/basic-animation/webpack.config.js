22babf2ebfff5e5a21d16b3ac3c8ef6aff8166b8
zachary_witt <zach.witt95@gmail.com>
2020-06-25 10:05:18 -0400

    Updated package.json and webpack.config.js to comply with other examples

--- examples/basic-animation/webpack.config.js
+++ examples/basic-animation/webpack.config.js
@@ -1,8 +1,11 @@
 var configure = require("react-figma-webpack-config");
+
 module.exports = configure({
   resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
     alias: {
-      "react-native$": "react-figma"
-    }
-  }
+      "react-figma$": "../../../src",
+      "react-figma/rpc$": "../../../src/rpc",
+    },
+  },
 });
