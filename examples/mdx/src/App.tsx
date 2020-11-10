ac7db06b482998a186e0c0065a292f4a4a16065f
Ilia Lesik <ilialesik@gmail.com>
2020-11-10 20:47:31 +0300

    Show components using in mdx example

--- examples/mdx/src/App.tsx
+++ examples/mdx/src/App.tsx
@@ -1,11 +1,46 @@
 import * as React from 'react';
-import { Page, View, Text } from 'react-figma';
+import { Page, View, Text, StyleSheet } from 'react-figma';
 const { MDXProvider } = require('@mdx-js/react');
-import Test from './test.mdx';
+import Hello from './hello.mdx';
+
+const styles = StyleSheet.create({
+    h1: {
+        fontFamily: 'Roboto',
+        fontSize: 32,
+        fontWeight: 'bold',
+        marginBottom: 24
+    },
+    p: {
+        fontFamily: 'Roboto',
+        fontSize: 16,
+        marginTop: 8
+    },
+    color: {
+        width: 32,
+        height: 32,
+        marginRight: 16
+    },
+    row: {
+        marginTop: 24,
+        flexDirection: 'row'
+    }
+});
+
+const Color = props => {
+    const { value } = props;
+    return <View style={[styles.color, { backgroundColor: value }]} />;
+};
+
+const Row = props => {
+    const { children } = props;
+    return <View style={styles.row}>{children}</View>;
+};
 
 const components = {
-    h1: ({ children }) => <Text style={{ fontFamily: 'Roboto', fontSize: 32 }}>{children}</Text>,
-    p: ({ children }) => <Text style={{ fontFamily: 'Roboto', fontSize: 16 }}>{children}</Text>
+    h1: ({ children }) => <Text style={styles.h1}>{children}</Text>,
+    p: ({ children }) => <Text style={styles.p}>{children}</Text>,
+    Color,
+    Row
 };
 
 export const App = () => {
@@ -13,7 +48,7 @@ export const App = () => {
         <Page name="New page" isCurrent>
             <View>
                 <MDXProvider components={components}>
-                    <Test />
+                    <Hello />
                 </MDXProvider>
             </View>
         </Page>
