edea00b1d59f1784854be7d3d0a7b601cceb9cce
Ilia Lesik <ilialesik@gmail.com>
2021-01-07 17:49:57 +0300

    Implement  A container with display: flex should get flexDirection: row;  alignItems: stretch for containers as default
    WEB defaults mode #377

--- src/components/view/__tests__/View.test.tsx
+++ src/components/view/__tests__/View.test.tsx
@@ -51,4 +51,30 @@ describe('<View />', () => {
             .toJSON();
         expect(tree).toMatchSnapshot();
     });
+
+    it('alignItems: "stretch" on web defaults mode', () => {
+        process.env.REACT_FIGMA_WEB_DEFAULTS_ENABLED = '1';
+        const tree = renderer
+            .create(
+                <View>
+                    <Text>text</Text>
+                </View>
+            )
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+        delete process.env.REACT_FIGMA_WEB_DEFAULTS_ENABLED;
+    });
+
+    it('flexDirection: "row" when display: flex on web defaults mode', () => {
+        process.env.REACT_FIGMA_WEB_DEFAULTS_ENABLED = '1';
+        const tree = renderer
+            .create(
+                <View style={{ display: 'flex' } as any}>
+                    <Text>text</Text>
+                </View>
+            )
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+        delete process.env.REACT_FIGMA_WEB_DEFAULTS_ENABLED;
+    });
 });
