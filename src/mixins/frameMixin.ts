89f635da038fc99767abec93bbc5351be76ade82
Kenneth Ah Young <kahyoung@fb.com>
2021-12-16 15:37:26 -0800

    Support fillStyleId for frames

--- src/mixins/frameMixin.ts
+++ src/mixins/frameMixin.ts
@@ -1,6 +1,6 @@
 import { propsAssign } from '../helpers/propsAssign';
 import { FrameProps } from '../types';
 
-export const frameMixin = propsAssign<FrameProps, FrameProps>(['backgrounds'], {
+export const frameMixin = propsAssign<FrameProps, FrameProps>(['backgrounds', 'fillStyleId'], {
     backgrounds: []
 });
