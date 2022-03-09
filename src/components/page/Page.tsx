6804b6870de8055b8eecf8f3f98fec88cdb3f6b8
Ilia Lesik <ilialesik@gmail.com>
2022-03-09 23:25:20 +0300

    Implement page background styling with style prop
    Add page backgound example

--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -6,9 +6,15 @@ import { $currentPageTempId, api } from '../../rpc';
 import { map } from 'rxjs/operators';
 import { OnLayoutHandlerProps, useOnLayoutHandler } from '../../hooks/useOnLayoutHandler';
 import { InheritStyleProvider } from '../../hooks/useInheritStyle';
+import {
+    GeometryStyleProperties,
+    transformGeometryStyleProperties
+} from "../../styleTransformers/transformGeometryStyleProperties";
+import {StyleSheet} from "../../helpers/StyleSheet";
+import {useImageHash} from "../../hooks/useImageHash";
 
 export interface PageProps extends BaseNodeProps, ChildrenProps, ExportProps, OnLayoutHandlerProps {
-    style?: StyleOf<YogaStyleProperties>;
+    style?: StyleOf<YogaStyleProperties & GeometryStyleProperties>;
     isCurrent?: boolean;
     onCurrentChange?: (isCurrent: boolean) => void;
     backgrounds?: ReadonlyArray<Paint>;
@@ -43,9 +49,19 @@ const Page: React.FC<PageProps> = props => {
     const yogaChildProps = useYogaLayout({ nodeRef, ...otherProps });
     useOnLayoutHandler(yogaChildProps, props);
 
+    const style = StyleSheet.flatten(props.style);
+
+    const imageHash = useImageHash(style.backgroundImage);
+
+    const pageProps = {
+        ...transformGeometryStyleProperties('backgrounds', style, imageHash),
+        ...otherProps,
+        style
+    };
+
     return (
         <InheritStyleProvider style={props.style}>
-            <page {...otherProps} {...yogaChildProps} innerRef={nodeRef} />
+            <page {...pageProps} {...yogaChildProps} innerRef={nodeRef} />
         </InheritStyleProvider>
     );
 };
