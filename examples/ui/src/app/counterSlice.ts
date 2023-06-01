cf79ef69cb8b69b1e65b72528e959f1dbb60e5e0
Ilia Lesik <ilialesik@gmail.com>
2023-06-02 00:03:20 +0300

    Add UI example

---
+++ examples/ui/src/app/counterSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState: {
+    value: 0
+  },
+  reducers: {
+    increment: state => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.value += 1
+    },
+    decrement: state => {
+      state.value -= 1
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload
+    }
+  }
+})
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount } = counterSlice.actions
+
+export default counterSlice.reducer
---
+++ examples/ui/src/app/store.ts
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './counterSlice'
+
+
+export default configureStore({
+  reducer: {
+    counter: counterReducer
+  }
+})
